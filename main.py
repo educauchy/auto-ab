940ec0a1deae87579a3ba1d9b63f185ed9818a1e
educauchy <educauchy@gmail.com>
2021-06-17 20:43:57 +0300

    Moved ABTest class to separate module

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
 import numpy as np
-import pandas as pd
-import statsmodels.stats.api as sms
-import math, os, sys, yaml
-import matplotlib.pyplot as plt
-from collections import Counter
-from scipy.stats import mannwhitneyu, ttest_1samp, ttest_ind, ttest_ind_from_stats, ttest_rel, t
-from typing import Dict, List, Tuple
+import os, sys, yaml, json
+from auto_ab import ABTest
 
 
 try:
@@ -21,196 +16,24 @@ except Exception as e:
     sys.exit(1)
 
 
+# Example scenario to use
+# x = np.random.normal(0, 1, 10000)
+# y = np.random.normal(0, 1, 20000)
+# m.use_datasets(x, y)
 
+m = ABTest(alpha=0.05, alternative='two-sided')
+X = np.random.randint(0, 300, 1000)
+m.use_dataset(X)
+m.set_increment(inc_var=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
+m.set_split_rate(split_rates=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+m.set_splitter()
+res = m.mde(n_iter=500, n_boot_samples=5, to_csv=True)
+print(json.dumps(res, indent=4))
 
-
-
-
-
-class ABTest:
-    """Perform AB-test"""
-    def __init__(self, alpha: float = 0.05, alternative: str = 'one-sided', n_boot_samples: int = 10000) -> None:
-        self.alpha = alpha
-        self.alternative = alternative
-        self.n_boot_samples = n_boot_samples
-        self.datasets = { 'A': {}, 'B': {}, 'type': 'continuous'}
-        self.power = {}
-        self.campaigns = {}
-
-    def _read_file(path: str) -> pd.DataFrame:
-        """Read file and return pandas dataframe"""
-        _, file_ext = os.path.splitext(path)
-        if file_ext == '.csv':
-            df = pd.read_csv(path, encoding='utf8')
-        elif file_ext == '.xls' or file_ext == '.xlsx':
-            df = pd.read_excel(path, encoding='utf8')
-        return df
-
-    def _generate_distribution(cls, dist_type: str, params: tuple, n_samples: int) -> np.array:
-        """Return distribution by type, with given parameters and number of samples."""
-        if dist_type == 'normal':
-            return np.random.normal(*params, n_samples)
-        elif dist_type == 'binomial':
-            return np.random.binomial(*params, n_samples)
-
-    def plot_distributions(self, save_path: str=None) -> None:
-        """Generate distributions and save plot on given path."""
-        bins = np.linspace(-10, 10, 100)
-        plt.hist(self.datasets['A']['data'], bins, alpha=0.5, label='control')
-        plt.hist(self.datasets['B']['data'], bins, alpha=0.5, label='treatment')
-        plt.legend(loc='upper right')
-
-        if save_path is not None:
-            plt.savefig(save_path)
-        else:
-            plt.show()
-
-    def ttest_ind(self, X: np.array, Y: np.array, equal_var: bool = False, use_bootstrap: bool = False) -> tuple:
-        """Perform T-test for independent samples with unequal number of observations and variance"""
-        T: int = 0
-        for _ in range(self.n_boot_samples):
-            x_boot = np.random.choice(self.datasets['X'], size=self.datasets['X'].shape[0], replace=True)
-            y_boot = np.random.choice(self.datasets['Y'], size=self.datasets['Y'].shape[0], replace=True)
-            T_boot = (np.mean(x_boot) - np.mean(y_boot)) / (np.var(x_boot)/np.size(x_boot) + np.var(y_boot)/np.size(y_boot))
-            test_res = ttest_ind(x_boot, y_boot, equal_var=equal_var, alternative=self.alternative)
-            if T_boot >= test_res[1]:
-                T += 1
-        boot_alpha = T / self.n_boot_samples
-        return boot_alpha
-
-    def use_datasets(self, X: np.array, Y: np.array) -> None:
-        """Load X and Y datasets"""
-        self.datasets['A'] = X
-        self.datasets['B'] = Y
-
-    def load_dataset(self, path: str, type: str='discrete', output: str=None,
-                     split_by: str=None, confound: str=None) -> None:
-        """Load dataset for AB-testing"""
-        self.datasets['type'] = type
-        df = self._read_file(path)
-
-        if 'timestamp' not in df.columns:
-            df['timestamp'] = range(df.shape[0])
-
-        if confound is None:
-            self.datasets['A']['data'] = df.loc[df[split_by] == 'control', output]
-            self.datasets['A']['timestamp'] = df.loc[df[split_by] == 'control', 'timestamp']
-            self.datasets['B']['data'] = df.loc[df[split_by] == 'treatment', output]
-            self.datasets['B']['timestamp'] = df.loc[df[split_by] == 'treatment', 'timestamp']
-
-    def generate_datasets(self, n_samples: int=20000, dist1: str='normal', dist1_params: tuple=(0, 1),
-                          dist2: str='normal', dist2_params: tuple=(2, 1.1),
-                          to_save: bool=False, save_path: str='./data/test_dataset.csv') -> None:
-        """Generate two datasets with given parameters for analysis."""
-        n_samples_each = n_samples // 2
-        a_response = [*self._generate_distribution(dist1, dist1_params, n_samples_each)]
-        b_response = [*self._generate_distribution(dist2, dist2_params, n_samples_each)]
-
-        # campaign_id = randint(1, 50)
-        campaign_id = np.random.randint(1, 50, 1)[0]
-        dataset = pd.DataFrame(columns=['user_id', 'campaign_id', 'group', 'response'])
-        dataset['user_id'] = range(n_samples)
-        dataset['group'] = ['control'] * n_samples_each + ['treatment'] * n_samples_each
-        dataset['response'] = a_response + b_response
-        dataset['timestamp'] = range(n_samples)
-        dataset = dataset.sample(frac=1)
-
-        self.datasets = {
-            'A': { 'data': a_response, 'timestamp': dataset['timestamp'][:n_samples_each].tolist() },
-            'B': { 'data': b_response, 'timestamp': dataset['timestamp'][n_samples_each:].tolist() }
-        }
-        self.campaigns[campaign_id] = dataset
-
-        if to_save:
-            dataset.to_csv(save_path, index=False)
-
-    def power_analysis(self, power: float=0.8, alpha: float=0.05, ratio: float=1.0,
-                       effect_size: float=None, n_samples: float=None) -> Dict[str, float]:
-        """Perform power analysis and return computed parameter which was initialised as None."""
-        self.alpha = alpha
-        unknown_arg = 'n_samples'
-        for arg in [*locals().keys()][1:]:
-            if eval(arg) is None:
-                unknown_arg = arg
-
-        result = sms.TTestIndPower().solve_power(
-            effect_size=effect_size,
-            power=power,
-            nobs1=n_samples,
-            alpha=alpha,
-            ratio=ratio
-        )
-        if unknown_arg == 'n_samples':
-            result = int(math.ceil(result))
-            self.min_sample_size = result
-        else:
-            result = round(result, 3)
-            self.min_sample_size = n_samples
-
-        output = {
-            'power': power,
-            'alpha': alpha,
-            'effect_size': effect_size,
-            'n_samples': n_samples,
-            'ratio': ratio,
-            'beta': None
-        }
-        output[unknown_arg] = result
-        output['beta'] = round(1 - output['power'], 3)
-
-        self.power = output
-        return output
-
-    def run_simulation(self, output_path: str='./data/sim_output.xlsx') -> None:
-        """Run simulations and save results into file."""
-        output = pd.DataFrame(columns=['iteration', 'control', 'treatment', 'statistic', 'pvalue', 'inference'])
-        A = pd.DataFrame(self.datasets['A'])
-        A['group'] = 'control'
-        B = pd.DataFrame(self.datasets['B'])
-        B['group'] = 'treatment'
-        dataset = pd.concat([A, B])
-        dataset.sort_values(by='timestamp', inplace=True)
-
-        for row_index in range(1, dataset.shape[0]):
-            series = {'iteration': row_index, 'control': 0, 'treatment': 0, 'statistic': '',
-                      'pvalue': '', 'inference': ''}
-            data = dataset.iloc[:row_index]
-            
-            groups = Counter(data['group'])
-            series = {**series, **groups}
-
-            if (groups['control'] < 20) or (groups['treatment'] < 20): # условие теста Манна-Уитни
-                continue
-            elif (groups['control'] > (self.min_sample_size) + 200) \
-                    and (groups['treatment'] > (self.min_sample_size + 200)):
-                break
-            else:
-                a = data.loc[data['group'] == 'control', 'data'].tolist()
-                b = data.loc[data['group'] == 'treatment', 'data'].tolist()
-                
-                series['statistic'], series['pvalue'] = mannwhitneyu(a, b, alternative='two-sided')
-                series['pvalue'] = round(series['pvalue'], 4)
-                series['inference'] = 'Same' if series['pvalue'] > self.alpha else 'Different'
-            output = output.append(pd.Series(series), ignore_index=True)
-
-        output.to_excel(output_path, index=False)
-
-if __name__ == '__main__':
-    # Example scenario to use
-    m = ABTest(alpha=0.5, alternative='two-sided', n_boot_samples=10000)
-    x = np.random.normal(0, 1, 10000)
-    y = np.random.normal(0, 1, 20000)
-    m.use_datasets(x, y)
-    # m.train_test_split(x, share_of_test_group=0.25)
-    # m.increase_B()
-    res = m.ttest_ind(equal_var=False, use_bootstrap=True)
-    print(res)
-
-
-    # m.generate_datasets(n_samples=config['n_samples'], dist1=config['dist1'], dist1_params=config['dist1_params'], \
-    #                     dist2=config['dist2'], dist2_params=config['dist2_params'])
-    # m.load_dataset('./data/test_dataset.csv', type='continuous', output='response', split_by='group', confound=None)
-    # m.plot_distributions(save_path='./output/AB_dists.png')
-    # print(m.power_analysis(n_samples=config['power']['n_samples'], effect_size=config['power']['effect_size'], power=None))
-    # m.run_simulation()
+# m.generate_datasets(n_samples=config['n_samples'], dist1=config['dist1'], dist1_params=config['dist1_params'], \
+#                     dist2=config['dist2'], dist2_params=config['dist2_params'])
+# m.load_dataset('./data/test_dataset.csv', type='continuous', output='response', split_by='group', confound=None)
+# m.plot_distributions(save_path='./output/AB_dists.png')
+# print(m.power_analysis(n_samples=config['power']['n_samples'], effect_size=config['power']['effect_size'], power=None))
+# m.run_simulation()
 
