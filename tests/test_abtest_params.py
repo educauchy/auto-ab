cddc53c018681399149bd90033733ef418cb5eb0
educauchy <educauchy@gmail.com>
2021-06-26 23:22:19 +0300

    Add AB-test parameters testing (alpha, alternative)

---
+++ tests/test_abtest_params.py
@@ -0,0 +1,25 @@
+from auto_ab import ABTest
+import unittest
+import pytest
+import scipy
+
+
+class TestABTestParams(unittest.TestCase):
+    def test_valid_alpha_alternative(self):
+        ABTest(alpha=0.05, alternative='less')
+
+    def test_invalid_alpha(self):
+        with pytest.raises(Exception) as e:
+            assert ABTest(alpha=1.05, alternative='less')
+        assert str(e.value) == "Significance level must be inside interval [0, 1]. Your input: 1.05."
+
+    def test_invalid_alternative(self):
+        with pytest.raises(Exception) as e:
+            assert ABTest(alpha=0.05, alternative='one-sided')
+        assert str(e.value) == "Alternative must be either 'less', 'greater', or 'two-sided'. Your input: 'one-sided'."
+
+    def test_power_range(self):
+        pass
+
+    def test_split_rates(self):
+        pass
--- tests/test_splitter.py
+++ tests/test_splitter.py
@@ -1,5 +1,5 @@
-import numpy as np
 from auto_ab import ABTest
+import numpy as np
 from typing import Dict, List, Tuple, Any, Union
 
 
@@ -7,12 +7,15 @@ class TestSplitter:
     def test_goodness_fit(self):
         def splitter_function(X: np.array) -> Tuple[Any, Any]:
             pass
+        def metric(X: np.array) -> float:
+            # return np.quantile(X, 0.1) # check while test fails on this metric
+            return np.mean(X)
 
-        m = ABTest(alpha=0.05, alternative='two-sided')
-        X = np.random.randint(0, 100, 100)
+        m = ABTest(alpha=0.05, alternative='less')
+        X = np.random.exponential(2, 100000)
         m.use_dataset(X)
         m.set_increment(inc_var=[0])
-        m.set_split_rate(split_rates=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9])
+        m.set_split_rate(split_rates=[0.1, 0.2, 0.3, 0.4, 0.5])
         m.set_splitter(splitter_function)
-        m.mde(n_iter=10, n_boot_samples=10, to_csv=True, csv_name='pytest_splitter')
-
+        m.mde(n_iter=10, n_boot_samples=50, metric=metric, n_buckets=100, test_type='buckets', to_csv=False,
+              csv_name='./auto_ab/data/splitter_10quantile_buckets.csv')
