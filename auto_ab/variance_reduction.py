d39254a955382ddea6dd75fe45f382eb1c1e5755
educauchy <educauchy@gmail.com>
2021-07-13 09:31:57 +0300

    Now covariate column can be derived if not specified

--- auto_ab/variance_reduction.py
+++ auto_ab/variance_reduction.py
@@ -1,12 +1,17 @@
 import numpy as np
 import pandas as pd
+from typing import Optional
 
 
 class VarianceReduction:
     def __init__(self, method: str = None):
         self.method = method
 
-    def cuped(self, X: pd.DataFrame, target: str = '', covariate: str = ''):
+    def cuped(self, X: pd.DataFrame, target: str = '', covariate: Optional[str] = None):
+        if covariate is None:
+            X_corr = X.select_dtypes(include=[np.number]).corr()
+            covariate = X_corr.loc[X_corr.index != target, target].sort_values(ascending=False).index[0]
+
         cov = X[[target, covariate]].cov().loc[target, covariate]
         var = X[covariate].var()
         self.theta = cov / var
@@ -15,10 +20,14 @@ class VarianceReduction:
         return X
 
 if __name__ == '__main__':
+    t = np.random.randint(160, 195, 200)
     df = pd.DataFrame({
-        't': np.random.randint(160, 195, 200), # height
-        'c': np.random.randint(60, 120, 200), # weight
+        't': t, # height
+        'c': t + np.random.randint(0, 100, 200), # height + random
+        'f': np.random.normal(0, 1, 200), # white noise
     })
+    df_copy = df.copy(deep=True)
 
     vr = VarianceReduction()
     print(vr.cuped(df, target='t', covariate='c'))
+    print(vr.cuped(df_copy, target='t', covariate=None))
