bde61d7e882cbf53041bf5aeefac2591b8396632
educauchy <educauchy@gmail.com>
2021-07-26 12:23:38 +0300

    Remove old functions, code refactoring, simple Welch t-test / Mann-Whitney test

--- auto_ab/abtest.py
+++ auto_ab/abtest.py
@@ -12,11 +12,9 @@ from .splitter import Splitter
 class ABTest:
     """Perform AB-test"""
     def __init__(self, alpha: float = 0.05, alternative: str = 'one-sided') -> None:
-        self.alpha = alpha # use self.__alpha everywhere in the class
-        self.alternative = alternative # use self.__alternative everywhere in the class
-        self.datasets = {'A': {}, 'B': {}, 'X': {}, 'type': 'continuous'}
-        self.power = {}
-        self.campaigns = {}
+        self.alpha = alpha                  # use self.__alpha everywhere in the class
+        self.alternative = alternative      # use self.__alternative everywhere in the class
+        self.dataset: pd.DataFrame = None
 
     @property
     def alpha(self):
@@ -62,7 +60,7 @@ class ABTest:
         X_with_groups = self.splitter.fit(X, split_rate)
         return X_with_groups
 
-    def _read_file(path: str) -> pd.DataFrame:
+    def _read_file(self, path: str) -> pd.DataFrame:
         """
         Read file and return pandas dataframe
         :param path: Path to file
@@ -76,18 +74,37 @@ class ABTest:
             df = pd.read_excel(path, encoding='utf8')
         return df
 
-    def _generate_distribution(cls, dist_type: str, params: tuple, n_samples: int) -> np.array:
+    def set_increment(self, inc_var: List[float] = None, extra_params: Dict[str, float] = None) -> None:
+        self.increment_list = inc_var
+        self.increment_extra = extra_params
+
+    def set_split_rates(self, split_rates: List[float] = None) -> None:
+        self.split_rates = split_rates
+
+    def set_splitter(self, splitter: Splitter) -> None:
         """
-        Return distribution by type, with given parameters and number of samples
-        :param dist_type: Distribution type (normal, binomial)
-        :param params: Params of a distribution
-        :param n_samples: Number of observations
-        :returns: Numpy array of desired distribution
+        Add splitter
+        :param splitter: Splitter instance of class Splitter
         """
-        if dist_type == 'normal':
-            return np.random.normal(*params, n_samples)
-        elif dist_type == 'binomial':
-            return np.random.binomial(*params, n_samples)
+        self.splitter = splitter
+
+    def use_dataset(self, X: np.array, target: str = None) -> None:
+        """
+        Put dataset for analysis
+        :param X: Dataset for analysis
+        :param target: Target column name
+        """
+        self.dataset = X
+        self.target = target
+
+    def load_dataset(self, path: str = '', target: str = None) -> None:
+        """
+        Load dataset for analysis
+        :param path: Path to the dataset for analysis
+        :param target: Target column name
+        """
+        self.dataset = self._read_file(path)
+        self.target = target
 
     def delta_method(self, Z: pd.DataFrame, numerator: str = '', denominator: str = '') -> int:
         pass
@@ -104,6 +121,22 @@ class ABTest:
         K = round(sum(X[numerator]) / sum(X[denominator]), 4)
         Z.loc[:, 'lnrzd_metric'] = Z[numerator] - K * Z[denominator]
         return Z
+
+    def test_hypothesis(self, X: np.array, Y: np.array) -> int:
+        """
+        Perform Welch's t-test / Mann-Whitney test for means/medians
+        :param X: Group A
+        :param Y: Group B
+        :return: Test result: 0 - cannot reject H0, 1 - reject H0
+        """
+        test_result: int = 0
+        if shapiro(X)[1] >= self.__alpha and shapiro(X)[1] >= self.__alpha:
+            _, pvalue = ttest_ind(X, Y, equal_var=False, alternative=self.__alternative)
+        else:
+            _, pvalue = mannwhitneyu(X, Y, alternative=self.__alternative)
+        if pvalue <= self.__alpha:
+            test_result = 1
+        return test_result
 
     def test_hypothesis_buckets(self, X: np.array, Y: np.array,
                                 metric: Optional[Callable[[Any], float]] = None,
@@ -226,7 +259,7 @@ class ABTest:
 
         return test_result
 
-    def test_hypothesis(self, X: np.array, Y: np.array, use_correction: bool = True) -> float:
+    def test_boot_hypothesis(self, X: np.array, Y: np.array, use_correction: bool = False) -> float:
         """
         Perform T-test for independent samples with unequal number of observations and variance
         :param X: Null hypothesis distribution
@@ -248,7 +281,7 @@ class ABTest:
         pvalue = T / self.n_boot_samples
         return pvalue
 
-    def mde(self, n_iter: int = 20000, strategy: str = 'means', strata: Optional[str] = '',
+    def mde_simulation(self, n_iter: int = 20000, strategy: str = 'means', strata: Optional[str] = '',
             n_boot_samples: Optional[int] = 10000, n_buckets: Optional[int] = None,
             metric: Optional[Callable[[Any], float]] = None, weights: Optional[Dict[str, float]] = None,
             use_correction: bool = True, to_csv: bool = False, csv_path: str = None) -> Dict[float, Dict[float, float]]:
@@ -269,7 +302,7 @@ class ABTest:
         if n_boot_samples < 1:
             raise Exception('Number of bootstrap samples must be 1 or more. Your input: {}.'.format(n_boot_samples))
         self.n_boot_samples = n_boot_samples
-        imitation_log = defaultdict(float)
+        imitation_log: Dict[float, Dict[float, int]] = defaultdict(float)
         csv_pd = pd.DataFrame()
         for split_rate in self.split_rates:
             imitation_log[split_rate] = {}
@@ -277,13 +310,16 @@ class ABTest:
                 imitation_log[split_rate][inc] = 0
                 for it in range(n_iter):
                     print(f'Split_rate: {split_rate}, inc_rate: {inc}, iter: {it}')
-                    X_with_groups = self._split_data(self.datasets['X'], split_rate)
+                    X_with_groups = self._split_data(self.dataset, split_rate)
                     control, treatment = X_with_groups.loc[X_with_groups['group'] == 'A', self.target].to_numpy(), \
                                          X_with_groups.loc[X_with_groups['group'] == 'B', self.target].to_numpy()
                     treatment = self._add_increment(treatment, inc)
 
-                    if strategy == 'means':
-                        pvalue: float = self.test_hypothesis(control, treatment, use_correction=use_correction)
+                    if strategy == 'simple_test':
+                        test_result: int = self.test_hypothesis(control, treatment)
+                        imitation_log[split_rate][inc] += test_result
+                    elif strategy == 'boot_hypothesis':
+                        pvalue: float = self.test_boot_hypothesis(control, treatment, use_correction=use_correction)
                         if pvalue <= self.__alpha:
                             imitation_log[split_rate][inc] += 1
                     elif strategy == 'boot_est':
@@ -311,154 +347,3 @@ class ABTest:
         if to_csv:
             csv_pd.to_csv(csv_path, index=False)
         return dict(imitation_log)
-
-    def use_datasets(self, X: np.array, Y: np.array) -> None:
-        """
-        Load X and Y datasets to use them in the test
-        :param X: Null hypothesis dataset
-        :param Y: Alternative hypothesis dataset
-        """
-        self.datasets['A'] = X
-        self.datasets['B'] = Y
-
-    def use_dataset(self, X: np.array, target: str = None) -> None:
-        """
-        Load X dataset for further splitting it into groups
-        :param X: Dataset to be splitted
-        :param target: Target column name
-        """
-        self.datasets['X'] = X
-        self.target = target
-
-    def load_dataset(self, path: str = '', data_type: str = 'discrete', target_col_name: str = None,
-                     split_by_col_name: str = None, confound_col_name: str = None) -> None:
-        """
-        Load dataset for splitting with splitting parameters
-        :param path: Path to the dataset
-        :param data_type: Type of data in dataset
-        :param target_col_name: Column name of the target variable
-        :param split_by_col_name: Column name of the stratified variable
-        :param confound_col_name: Column name of the confound variable
-        """
-        self.datasets['type'] = data_type
-        df = self._read_file(path)
-
-        if 'timestamp' not in df.columns:
-            df['timestamp'] = range(df.shape[0])
-
-        if confound_col_name is None:
-            self.datasets['A']['data'] = df.loc[df[split_by_col_name] == 'control', target_col_name]
-            self.datasets['A']['timestamp'] = df.loc[df[split_by_col_name] == 'control', 'timestamp']
-            self.datasets['B']['data'] = df.loc[df[split_by_col_name] == 'treatment', target_col_name]
-            self.datasets['B']['timestamp'] = df.loc[df[split_by_col_name] == 'treatment', 'timestamp']
-
-    def generate_datasets(self, n_samples: int = 20000, dist1: str = 'normal', dist1_params: tuple = (0, 1),
-                          dist2: str = 'normal', dist2_params: tuple = (2, 1.1),
-                          to_save: bool = False, save_path: str = './data/test_dataset.csv') -> None:
-        """Generate two datasets with given parameters for analysis."""
-        n_samples_each = n_samples // 2
-        a_response = [*self._generate_distribution(dist1, dist1_params, n_samples_each)]
-        b_response = [*self._generate_distribution(dist2, dist2_params, n_samples_each)]
-
-        # campaign_id = randint(1, 50)
-        campaign_id = np.random.randint(1, 50, 1)[0]
-        dataset = pd.DataFrame(columns=['user_id', 'campaign_id', 'group', 'response'])
-        dataset['user_id'] = range(n_samples)
-        dataset['group'] = ['control'] * n_samples_each + ['treatment'] * n_samples_each
-        dataset['response'] = a_response + b_response
-        dataset['timestamp'] = range(n_samples)
-        dataset = dataset.sample(frac=1)
-
-        self.datasets = {
-            'A': {'data': a_response, 'timestamp': dataset['timestamp'][:n_samples_each].tolist()},
-            'B': {'data': b_response, 'timestamp': dataset['timestamp'][n_samples_each:].tolist()}
-        }
-        self.campaigns[campaign_id] = dataset
-
-        if to_save:
-            dataset.to_csv(save_path, index=False)
-
-    def power_analysis(self, power: float = 0.8, alpha: float = 0.05, ratio: float = 1.0,
-                       effect_size: float = None, n_samples: float = None) -> Dict[str, float]:
-        """Perform power analysis and return computed parameter which was initialised as None."""
-        self.__alpha = alpha
-        unknown_arg = 'n_samples'
-        for arg in [*locals().keys()][1:]:
-            if eval(arg) is None:
-                unknown_arg = arg
-
-        result = sms.TTestIndPower().solve_power(
-            effect_size=effect_size,
-            power=power,
-            nobs1=n_samples,
-            alpha=alpha,
-            ratio=ratio
-        )
-        if unknown_arg == 'n_samples':
-            result = int(math.ceil(result))
-            self.min_sample_size = result
-        else:
-            result = round(result, 3)
-            self.min_sample_size = n_samples
-
-        output = {
-            'power': power,
-            'alpha': alpha,
-            'effect_size': effect_size,
-            'n_samples': n_samples,
-            'ratio': ratio,
-            'beta': None
-        }
-        output[unknown_arg] = result
-        output['beta'] = round(1 - output['power'], 3)
-
-        self.power = output
-        return output
-
-    def run_simulation(self, output_path: str = './data/sim_output.xlsx') -> None:
-        """Run simulations and save results into file."""
-        output = pd.DataFrame(columns=['iteration', 'control', 'treatment', 'statistic', 'pvalue', 'inference'])
-        A = pd.DataFrame(self.datasets['A'])
-        A['group'] = 'control'
-        B = pd.DataFrame(self.datasets['B'])
-        B['group'] = 'treatment'
-        dataset = pd.concat([A, B])
-        dataset.sort_values(by='timestamp', inplace=True)
-
-        for row_index in range(1, dataset.shape[0]):
-            series = {'iteration': row_index, 'control': 0, 'treatment': 0, 'statistic': '',
-                      'pvalue': '', 'inference': ''}
-            data = dataset.iloc[:row_index]
-
-            groups = Counter(data['group'])
-            series = {**series, **groups}
-
-            if (groups['control'] < 20) or (groups['treatment'] < 20):  # условие теста Манна-Уитни
-                continue
-            elif (groups['control'] > (self.min_sample_size) + 200) \
-                    and (groups['treatment'] > (self.min_sample_size + 200)):
-                break
-            else:
-                a = data.loc[data['group'] == 'control', 'data'].tolist()
-                b = data.loc[data['group'] == 'treatment', 'data'].tolist()
-
-                series['statistic'], series['pvalue'] = mannwhitneyu(a, b, alternative='two-sided')
-                series['pvalue'] = round(series['pvalue'], 4)
-                series['inference'] = 'Same' if series['pvalue'] > self.__alpha else 'Different'
-            output = output.append(pd.Series(series), ignore_index=True)
-        output.to_excel(output_path, index=False)
-
-    def set_increment(self, inc_var: List[float] = None, extra_params: Dict[str, float] = None) -> None:
-        self.increment_list = inc_var
-        self.increment_extra = extra_params
-
-    def set_split_rate(self, split_rates: List[float] = None) -> None:
-        self.split_rates = split_rates
-
-    def set_splitter(self, splitter: Splitter) -> None:
-        """
-        Add splitter
-        :param splitter: Splitter instance of class Splitter
-        """
-        self.splitter = splitter
-
