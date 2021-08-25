9c8ef2529ded950fb86fbc359240db58480fbde6
educauchy <educauchy@gmail.com>
2021-08-25 14:36:30 +0400

    Add first version of user guide, add simulation plot method

--- auto_ab/graphics.py
+++ auto_ab/graphics.py
@@ -1,28 +1,37 @@
-import seaborn as sns
-import matplotlib.pyplot as plt
 import numpy as np
-from typing import Union
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from typing import Union, Dict
 
 
 class Graphics:
-    def __init__(self):
+    def __init__(self) -> None:
         pass
 
-    def plot_distributions(self, save_path: str) -> None:
-        """Generate distributions and save plot on given path."""
-        bins = np.linspace(-10, 10, 100)
-        plt.hist(self.datasets['A']['data'], bins, alpha=0.5, label='control')
-        plt.hist(self.datasets['B']['data'], bins, alpha=0.5, label='treatment')
-        plt.legend(loc='upper right')
-        plt.savefig(save_path)
-
-
-    def plot_distribution(self, X: Union[np.array], ci: np.array, save_path: str) -> None:
-        """Generate distributions and save plot on given path."""
-        bins = np.linspace(-10, 10, 100)
-        plt.hist(X, bins, alpha=0.9, label='Custom metric distribution')
-        plt.vlines(ci, ymin=0, ymax=20, linestyle='--')
-        plt.legend(loc='upper right')
-        plt.savefig(save_path)
+    @staticmethod
+    def plot_simulation_log(log_path: str):
+        df = pd.read_csv(log_path)
+        df_pivot = df.pivot(index='split_rate', columns='increment', values='pval_sign_share')
+        plt.figure(figsize=(15, 8))
+        plt.title('Simulation log')
+        sns.heatmap(df_pivot, cmap='Greens', annot=True)
+        plt.show()
         plt.close()
 
+    # def plot_distributions(self, save_path: str) -> None:
+    #     """Generate distributions and save plot on given path."""
+    #     bins = np.linspace(-10, 10, 100)
+    #     plt.hist(self.datasets['A']['data'], bins, alpha=0.5, label='control')
+    #     plt.hist(self.datasets['B']['data'], bins, alpha=0.5, label='treatment')
+    #     plt.legend(loc='upper right')
+    #     plt.savefig(save_path)
+    #
+    # def plot_distribution(self, X: Union[np.array], ci: np.array, save_path: str) -> None:
+    #     """Generate distributions and save plot on given path."""
+    #     bins = np.linspace(-10, 10, 100)
+    #     plt.hist(X, bins, alpha=0.9, label='Custom metric distribution')
+    #     plt.vlines(ci, ymin=0, ymax=20, linestyle='--')
+    #     plt.legend(loc='upper right')
+    #     plt.savefig(save_path)
+    #     plt.close()
