be26505f58d54d8e897ba9b74c9fe67af2e03cac
educauchy <educauchy@gmail.com>
2022-03-24 22:18:00 +0300

    Add experiment plot

---
+++ app/auto_ab/graphics.py
@@ -0,0 +1,70 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+from typing import Union, Dict, List
+from scipy.stats import norm, mode
+
+
+class Graphics:
+    def __init__(self) -> None:
+        pass
+
+    @staticmethod
+    def plot_simulation_log(log_path: str):
+        df = pd.read_csv(log_path)
+        df_pivot = df.pivot(index='split_rate', columns='increment', values='pval_sign_share')
+        plt.figure(figsize=(15, 8))
+        plt.title('Simulation log')
+        sns.heatmap(df_pivot, cmap='Greens', annot=True)
+        plt.show()
+        plt.close()
+
+    @staticmethod
+    def plot_distributions(X: pd.DataFrame, target: str, groups: str, bins: int = 30) -> None:
+        """Plot distributions and save plot on given path."""
+        a = X.loc[X[groups] == 'A', target]
+        b = X.loc[X[groups] == 'B', target]
+        a_mean = a.mean()
+        b_mean = b.mean()
+        plt.hist(a, bins, alpha=0.5, label='control')
+        plt.hist(b, bins, alpha=0.5, label='treatment')
+        plt.vlines([a_mean, b_mean], ymin=0, ymax=1000, linestyle='--')
+        plt.legend(loc='upper right')
+        plt.show()
+        plt.close()
+
+    def plot_distribution(self, X: Union[np.array], ci: np.array = None, bins: int = 30) -> None:
+        """Generate distributions and save plot on given path."""
+        plt.hist(X, bins, alpha=0.9, label='Custom metric distribution')
+        if ci is not None:
+            plt.vlines(ci, ymin=0, ymax=20, linestyle='--')
+        plt.legend(loc='upper right')
+        plt.show()
+        plt.close()
+
+    @staticmethod
+    def plot_experiment(a: Union[np.array, List[Union[int, float]]] = None,
+                            b: Union[np.array, List[Union[int, float]]] = None,
+                            alternative: str = 'two-sided',
+                            metric: str = 'mean', alpha: float = 0.05, beta: float = 0.2) -> None:
+        bins = 50
+        a_mean = np.mean(a)
+        b_mean = np.mean(b)
+        threshold = np.quantile(a, 0.975)
+        fig, ax = plt.subplots(figsize=(20, 12))
+        ax.text(a_mean, 100, 'H0', fontsize='xx-large')
+        ax.text(b_mean, 100, 'H1', fontsize='xx-large')
+        ax.hist(a, bins, alpha=0.5, label='control', color='Red')
+        ax.hist(b, bins, alpha=0.5, label='treatment', color='Green')
+        ax.axvline(x=a_mean, color='Red')
+        ax.axvline(x=b_mean, color='Green')
+        ax.axvline(x=threshold, color='Blue', label='critical value')
+        ax.legend()
+        plt.show()
+
+if __name__ == '__main__':
+    a = np.random.normal(0, 8, 5_000)
+    b = np.random.normal(25, 6, 5_000)
+    gr = Graphics()
+    gr.plot_experiment(a, b)
